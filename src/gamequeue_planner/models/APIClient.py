1618c6ee874140852d8f5e419da095c041f8d17d
Adam Chomicki <admchm@outlook.com>
2024-07-12 14:21:17 +0200

    Fix possible error for getting the specific platform release date for a game that was released ona a couple of platforms

--- src/gamequeue_planner/models/APIClient.py
+++ src/gamequeue_planner/models/APIClient.py
@@ -10,7 +10,19 @@ class APIClient(object):
             config = json.load(config_file)
         api_key = config['api_key']
         
-        api_url = f"https://api.mobygames.com/v1/games/37067?format=normal&api_key={api_key}"
+        #selected_platform = "SEGA Saturn"
+        #selected_platform_id = platforms['SEGA Saturn']
+        
+        selected_platform = "SNES"
+        selected_platform_id = platforms['SNES']
+        
+        #selected_platform = "Nintendo DS"
+        #selected_platform_id = platforms['Nintendo DS']
+        
+        print(selected_platform_id)
+        
+        #api_url = f"https://api.mobygames.com/v1/games/37067?format=normal&api_key={api_key}"
+        api_url = f"https://api.mobygames.com/v1/games/78?format=normal&api_key={api_key}"
 
         #req = requests.get(api_url)
         #data = req.json()
@@ -27,14 +39,20 @@ class APIClient(object):
         single_game.moby_score = data['moby_score']
         single_game.moby_num_votes = data['num_votes']
 
-        # TODO: - Possible error; verify the behavior for game that was released for a couple of platforms
         platform_details = data['platforms'] # getting nested data
-        single_game.first_release_date = platform_details[0]['first_release_date']
-        single_game.platform_name = platform_details[0]['platform_name']
-        single_game.platform_id = platform_details[0]['platform_id']
         
+        i = 0 
+        platform_not_found = True
+        
+        while platform_not_found:
+            if platform_details[i]['platform_name'] == selected_platform:
+                single_game.first_release_date = platform_details[i]['first_release_date']
+                single_game.platform_name = platform_details[i]['platform_name']
+                single_game.platform_id = platform_details[i]['platform_id']
+                platform_not_found = False
+            else:
+                i += 1
+                 
         single_game.print_details()
         
-        #pprint(data)
-
-#print(platforms["PlayStation"])
+        return single_game
