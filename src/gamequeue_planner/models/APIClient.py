bf7dbbff79d0e4aa8958468a0c4e5f7af28155ac
Adam Chomicki <admchm@outlook.com>
2024-07-12 13:10:54 +0200

    Store game details as an instance

--- src/gamequeue_planner/models/APIClient.py
+++ src/gamequeue_planner/models/APIClient.py
@@ -1,7 +1,8 @@
 import requests
 import json
-from pprint import pprint
+
 from models.platforms import platforms
+from models.GameObject import GameObject
 
 class APIClient(object):
     def fetch_data_from_API(self):
@@ -9,7 +10,6 @@ class APIClient(object):
             config = json.load(config_file)
         api_key = config['api_key']
         
-        print("##################")
         api_url = f"https://api.mobygames.com/v1/games/37067?format=normal&api_key={api_key}"
 
         #req = requests.get(api_url)
@@ -20,6 +20,21 @@ class APIClient(object):
         "course_id": 1, "full": "true" })
 
         data = r.json()
-        pprint(data)
+              
+        single_game = GameObject()
+        single_game.title = data['title']
+        single_game.game_id = data['game_id']
+        single_game.moby_score = data['moby_score']
+        single_game.moby_num_votes = data['num_votes']
+
+        # TODO: - Possible error; verify the behavior for game that was released for a couple of platforms
+        platform_details = data['platforms'] # getting nested data
+        single_game.first_release_date = platform_details[0]['first_release_date']
+        single_game.platform_name = platform_details[0]['platform_name']
+        single_game.platform_id = platform_details[0]['platform_id']
+        
+        single_game.print_details()
+        
+        #pprint(data)
 
 #print(platforms["PlayStation"])
--- src/gamequeue_planner/models/GameObject.py
+++ src/gamequeue_planner/models/GameObject.py
@@ -1,14 +1,19 @@
 class GameObject(object):
     
-    def _init__(self, title, game_id, first_release_date, moby_score, moby_num_votes, platform_name, platform_id):
+    def __init__(self, title='', game_id='', moby_score='', moby_num_votes='', first_release_date='', platform_name='', platform_id=''):
         self.title = title
         self.game_id = game_id
-        self.first_release_date = first_release_date
         self.moby_score = moby_score
         self.moby_num_votes = moby_num_votes
+        self.first_release_date = first_release_date
         self.platform_name = platform_name
         self.platform_id = platform_id
         
     def print_details(self):
-        print("Fetched data:")
         print(f"title: {self.title}")
+        print(f"game id: {self.game_id}")
+        print(f"moby score: {self.moby_score}")
+        print(f"moby num votes: {self.moby_num_votes}")
+        print(f"first release date: {self.first_release_date}")
+        print(f"platform name: {self.platform_name}")
+        print(f"platform id: {self.platform_id}")
