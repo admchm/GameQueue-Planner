e47c4cc13577570537b5c839624da111281317d5
Adam Chomicki <admchm@outlook.com>
2024-07-12 11:43:41 +0200

    Handle the load of the api_key from config file

--- src/gamequeue_planner/models/APIClient.py
+++ src/gamequeue_planner/models/APIClient.py
@@ -1,12 +1,17 @@
 import requests
+import json
 from pprint import pprint
 from models.platforms import platforms
 
+
 class APIClient(object):
-    
     def fetch_data_from_API(self):
+        with open('config_file.json', 'r') as config_file:
+            config = json.load(config_file)
+        api_key = config['api_key']
+        
         print("##################")
-        api_url = "https://api.mobygames.com/v1/games/37067?format=normal&api_key="
+        api_url = "https://api.mobygames.com/v1/games/37067?format=normal&api_key={api_key}"
 
         #req = requests.get(api_url)
         #data = req.json()
