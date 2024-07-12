a37f12027de570e523aca93e27a85153c8eb3d95
Adam Chomicki <admchm@outlook.com>
2024-07-12 11:19:14 +0200

    Get single game

--- src/gamequeue_planner/main.py
+++ src/gamequeue_planner/main.py
@@ -1,21 +1,4 @@
-from pprint import pprint
-import requests
+from models.APIClient import APIClient
 
-from models.platforms import platforms
-
-api_url = "https://api.mobygames.com/v1/games/37067?format=normal&api_key="
-
-req = requests.get(api_url)
-data = req.json()
-
-pprint(data)
-
-# alter this code tomorrow
-r = requests.get(api_url, params={
-    "course_id": 1, "full": "true" })
-
-data = r.json()
-pprint(data)
-
-
-#print(platforms["PlayStation"])
+client = APIClient()
+client.fetch_data_from_API()
---
+++ src/gamequeue_planner/models/APIClient.py
@@ -0,0 +1,22 @@
+import requests
+from pprint import pprint
+from models.platforms import platforms
+
+class APIClient(object):
+    
+    def fetch_data_from_API(self):
+        print("##################")
+        api_url = "https://api.mobygames.com/v1/games/37067?format=normal&api_key=moby_"
+
+        req = requests.get(api_url)
+        data = req.json()
+
+        pprint(data)
+
+        r = requests.get(api_url, params={
+        "course_id": 1, "full": "true" })
+
+        data = r.json()
+        pprint(data)
+
+#print(platforms["PlayStation"])
