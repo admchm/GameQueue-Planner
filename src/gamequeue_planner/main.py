644312bc503ba2dd9aaeab6bd4411bebbb091063
Adam Chomicki <admchm@outlook.com>
2024-07-16 12:01:10 +0200

    Take out the functionality from APIClient to main

--- src/gamequeue_planner/main.py
+++ src/gamequeue_planner/main.py
@@ -1,4 +1,16 @@
 from models.APIClient import APIClient
+from models.DatesEditor import DatesEditor
+from models.GamesListEditor import GamesListEditor
+from models.CSVCreator import CSVCreator
 
 client = APIClient()
-client.fetch_data_from_API()
+games_data = client.fetch_data_from_API()
+
+dates_editor = DatesEditor()
+dates_editor.fix_the_dates_if_needed(games_data)
+
+games_list_editor = GamesListEditor()
+games_array = games_list_editor.sort_by_date(games_data)
+
+csv = CSVCreator()
+csv.prepare_file(games_data)
--- src/gamequeue_planner/models/APIClient.py
+++ src/gamequeue_planner/models/APIClient.py
@@ -5,41 +5,24 @@ import time
 from models.Platform import Platform
 from models.platform_ids import platform_ids
 from models.GameObject import GameObject
-from models.CSVCreator import CSVCreator
-from models.DatesEditor import DatesEditor
-from models.GamesListEditor import GamesListEditor
 
 class APIClient(object):
-    def fix_dates(self, games_array):
-        dates_editor = DatesEditor()
-        games_array = dates_editor.fix_the_dates_if_needed(games_array)
-        
-        return games_array
     
-    def sort_data(self, games_array):
-        games_list_editor = GamesListEditor()
-        games_array = games_list_editor.sort_by_date(games_array)
-        
-        return games_array
-    
-    def create_CSV_file(self, games_array):
-        csv = CSVCreator()
-        csv.prepare_file(games_array)
+    def __init__(self, offset=1200, limit=100, games_array=[]):
+        self.offset = offset
+        self.limit = limit
+        self.games_array = games_array
     
     def fetch_data_from_API(self):
         with open('config_file.json', 'r') as config_file:
             config = json.load(config_file)
         api_key = config['api_key']
         
-        selected_platform = Platform.Xbox_360.value
+        selected_platform = Platform.SNES.value # TODO: - extract this value a level higher
         selected_platform_id = platform_ids[selected_platform]
-
-        offset = 0
-        limit =  100    
-        games_array = []
         
         while True:
-            api_url = f"https://api.mobygames.com/v1/games?platform={selected_platform_id}&format=normal&offset={offset}&api_key={api_key}"
+            api_url = f"https://api.mobygames.com/v1/games?platform={selected_platform_id}&format=normal&offset={self.offset}&api_key={api_key}"
             
             r = requests.get(api_url)
             data = r.json()['games']
@@ -69,12 +52,9 @@ class APIClient(object):
                         i += 1
                         
                 single_game.print_details()
-                games_array.append(single_game)
+                self.games_array.append(single_game)
                 
             time.sleep(10)
-            offset += limit
-        print(len(games_array))
+            self.offset += self.limit
         
-        games_array = self.fix_dates(games_array)
-        games_array = self.sort_data(games_array)
-        self.create_CSV_file(games_array)
+        return self.games_array
