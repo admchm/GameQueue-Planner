4a138a9bcad3cf0a5e2971d7a2d7bf228e96e0fd
Adam Chomicki <admchm@outlook.com>
2024-07-18 15:21:33 +0200

    Cover more cases for sorting games lists

--- tests/GamesListEditor_test.py
+++ tests/GamesListEditor_test.py
@@ -17,8 +17,24 @@ def game_list():
         MockGame("Third game", "1990-03-07")
     ]
     
-def test_sort_by_date(game_list):
+def test_sort_by_date_order(game_list):
     editor = GamesListEditor()
     sorted_games = editor.sort_by_date(game_list)
     
     assert [game.title for game in sorted_games] == ["Second game", "Third game", "First game"]
+    
+def test_sort_by_date_empty_list(game_list):
+    editor = GamesListEditor()
+    sorted_games = editor.sort_by_date([])
+    
+    assert sorted_games == []
+    
+def test_sort_by_date_invalid_date_format(game_list):
+    editor = GamesListEditor()
+    
+    games_with_invalid_date_format = [
+        MockGame("First correct game", "2007-03-22"),
+        MockGame("Second incorrect game", "27-03-2010")
+    ]
+    with pytest.raises(ValueError):
+        editor.sort_by_date(games_with_invalid_date_format)
