0bcb6fcb9067deaad5ee16d157416a59b891d9bf
Adam Chomicki <admchm@outlook.com>
2024-07-22 10:35:07 +0200

    Remove unused imports

--- tests/DatesEditor_test.py
+++ tests/DatesEditor_test.py
@@ -1,13 +1,9 @@
-import sys
-import os
-
 import pytest
 
 # I decided to use freezgun for testing dates. It seems not that crucial for now,
 # but in the past I was using different format with hh-mm-ss. I'm not sure if I
 # won't use it again in the future, and that's why I'm including it:
 from freezegun import freeze_time
-from datetime import datetime
 from src.gamequeue_planner.models.DatesEditor import DatesEditor
 
 @freeze_time("2023-07-19")
