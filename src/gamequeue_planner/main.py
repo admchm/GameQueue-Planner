7bec14a86ed897468f334cc7cdabfbb04b7e1d60
Adam Chomicki <admchm@outlook.com>
2024-07-24 12:10:36 +0200

    Filter data from special edition and downloadable content

--- src/gamequeue_planner/main.py
+++ src/gamequeue_planner/main.py
@@ -8,11 +8,16 @@ from models.Platform import Platform
 from models.SelectedFileFormat import SelectedFileFormat
 from models.ExcelFileCreator import ExcelFileCreator
 
-#selected_platforms = [Platform.PSP.value, Platform.Nintendo_DS.value]
-selected_platforms = [Platform.SEGA_32X.value, Platform.SEGA_CD.value]
-
+selected_platforms = [Platform.PSP.value, Platform.Nintendo_DS.value]
+#selected_platforms = [Platform.SEGA_32X.value, Platform.SEGA_CD.value]
 
 fetched_data = []
+
+def filter_excluding_special_edition(fetched_data_with_correct_dates):
+    return [game for game in fetched_data_with_correct_dates if not game.is_special_edition]
+
+def filter_excluding_dlcs(partially_filtered_data):
+    return [game for game in partially_filtered_data if not game.is_DLC]
 
 for platform in selected_platforms:
     client = APIClient()
@@ -25,8 +30,13 @@ for platform in selected_platforms:
 dates_editor = DatesEditor()
 fetched_data_with_correct_dates = dates_editor.fix_the_dates_if_needed(fetched_data)
  
+
+# TODO: - both functions are temporary and will be removed after implementing database
+partially_filtered_data = filter_excluding_special_edition(fetched_data_with_correct_dates)
+filtered_data = filter_excluding_dlcs(partially_filtered_data)
+
 games_list_editor = GamesListEditor()
-sorted_data = games_list_editor.sort_by_date(fetched_data_with_correct_dates)
+sorted_data = games_list_editor.sort_by_date(filtered_data)
 
 file_extension = SelectedFileFormat.EXCEL
 
